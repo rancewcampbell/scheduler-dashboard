8608adc86d0565b4c6519e15551ad65663ab50ea
Rance Campbell <rancecampbell@Rances-MBP.hitronhub.home>
2020-05-21 13:59:29 -0600

    add panel logic to focus and unfocus a panel

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -32,27 +32,40 @@ class Dashboard extends Component {
 
     this.state = {
       loading: false,
+      focused: null,
     };
+
+    this.selectPanel = this.selectPanel.bind(this);
   }
+
+  selectPanel(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id,
+    }));
+  }
+
   render() {
-    const dashboardClasses = classnames('dashboard');
+    const dashboardClasses = classnames('dashboard', {
+      'dashboard--focused': this.state.focused,
+    });
+
+    const panels = data
+      .filter(el => this.state.focused === null || this.state.focused === el.id)
+      .map(el => (
+        <Panel
+          onSelect={this.selectPanel}
+          value={el.value}
+          labe={el.label}
+          key={el.id}
+          id={el.id}
+        />
+      ));
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    return (
-      <main className={dashboardClasses}>
-        {data.map(el => (
-          <Panel
-            value={el.value}
-            labe={el.label}
-            key={el.id}
-            id={el.id}
-          ></Panel>
-        ))}
-      </main>
-    );
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 
 export default class Panel extends Component {
   render() {
-    const { label, value } = this.props;
+    const { label, value, onSelect, id } = this.props;
     return (
-      <section className="dashboard__panel">
+      <section onClick={e => onSelect(id)} className="dashboard__panel">
         <h1 className="dashboard__panel-header">{label}</h1>
         <p className="dashboard__panel-value">{value}</p>
       </section>
