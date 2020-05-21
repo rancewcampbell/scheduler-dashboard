bb85e804670ca7248f0147d040496356b3a2433b
Rance Campbell <rancecampbell@Rances-MBP.hitronhub.home>
2020-05-21 13:27:20 -0600

    add Panel component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
 import Loading from './Loading';
+import Panel from './Panel';
 import classnames from 'classnames';
+
+const data = [
+  {
+    id: 1,
+    label: 'Total Interviews',
+    value: 6,
+  },
+  {
+    id: 2,
+    label: 'Least Popular Time Slot',
+    value: '1pm',
+  },
+  {
+    id: 3,
+    label: 'Most Popular Day',
+    value: 'Wednesday',
+  },
+  {
+    id: 4,
+    label: 'Interviews Per Day',
+    value: '2.3',
+  },
+];
 
 class Dashboard extends Component {
   constructor(props) {
@@ -12,10 +36,23 @@ class Dashboard extends Component {
   }
   render() {
     const dashboardClasses = classnames('dashboard');
+
     if (this.state.loading) {
       return <Loading />;
     }
-    return <main className={dashboardClasses}></main>;
+
+    return (
+      <main className={dashboardClasses}>
+        {data.map(el => (
+          <Panel
+            value={el.value}
+            labe={el.label}
+            key={el.id}
+            id={el.id}
+          ></Panel>
+        ))}
+      </main>
+    );
   }
 }
 
